82c4358f3c71079415ac109c195d075ff6c4baaa
Darkhanova <91181096+Darkhanova@users.noreply.github.com>
2021-11-17 02:18:51 +0800

    Update Operations.cs

--- WindowsFormsApp2/Operations.cs
+++ WindowsFormsApp2/Operations.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace WindowsFormsApp2
     {
         private int _znam, _chisl;
         private String str;
+
         public int znam { get => _znam; }
         public int chisl { get => _chisl; }
         public String getStr { get => str; }
@@ -18,21 +19,17 @@ namespace WindowsFormsApp2
         {
             _znam = z;
             _chisl = ch;
-
         }
 
         public Drob(String text)
         {
             str = text;
-
         }
 
         public String output()
         {
             return _chisl + "/" + _znam;
         }
-
-        
 
         public static Drob operator +(Drob drob1, Drob drob2)
         {
@@ -52,71 +49,24 @@ namespace WindowsFormsApp2
         }
         public static Drob operator %(Drob drob1, Drob drob2)
         {
-            int znam = 0, chisl = 0;
-            if (drob1.znam < drob1.znam)
+            
+            for(int i = 2; i <= drob1.znam; i++)
             {
-                Console.WriteLine("Ошибка!");
-
-            }
-            else
-            {
-
-                for (int i = 2; i < 11; i++)
+                if(drob1.znam % i == 0 && drob1.chisl % i == 0)
                 {
-                    if (drob1.znam % i == 0 && drob1.chisl % i == 0)
-                    {
-                        znam = drob1.znam / i;
-                        chisl = drob1.chisl / i;
-                    }
-                    else;
+                    drob1._znam = drob1.znam / i;
+                    drob1._chisl = drob1.chisl / i;
                 }
-                for (int i = 2; i < 11; i++)
-                {
-                    if (znam % i == 0 && chisl % i == 0)
-                    {
-                        znam = drob1.znam / i;
-                        chisl = drob1.chisl / i;
-                    }
-                    else;
-
-                }
-
             }
-            return new Drob(chisl, znam);
+            return new Drob(drob1._chisl, drob1._znam);
         }
 
         public static Drob operator ^(Drob drob1, Drob drob2)
         {
-            if ((double) drob1.chisl / (double)drob1.znam == (double)drob2.chisl / (double)drob2.znam)
+            if ((double)drob1.chisl / (double)drob1.znam == (double)drob2.chisl / (double)drob2.znam)
                 return new Drob("Равны");
             else
                 return new Drob("Не равны");
         }
-
-        public void showDrob()
-        {
-            Console.Out.WriteLine(chisl + "/" + znam);
-        }
-
-
-
     }
-
-
-
 }
-
-
-
-
-
-
-        
-        
-
-
-
-    
-
-
-
